7445dbf56e1d5428028e06022c9e88004a947c12
arc968 <andrewcourtemanche968@gmail.com>
2021-11-03 15:51:19 -0400

    Boilerplate

---
+++ src/hal/hal.h
@@ -0,0 +1,11 @@
+#ifndef HAL_H
+#define HAL_H
+
+#include "hal_gpio.h"
+#include "hal_i2c.h"
+#include "hal_interrupt.h"
+#include "hal_serial.h"
+#include "hal_spi.h"
+#include "hal_timer.h"
+
+#endif // HAL_H
