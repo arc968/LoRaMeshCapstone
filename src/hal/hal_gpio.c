7b226cc560596227b5151a0b684524e52b2e649c
joshLariviere <joshlariv@gmail.com>
2021-11-04 14:44:27 -0400

    boilerplate

--- src/hal/hal_gpio.c
+++ src/hal/hal_gpio.c
@@ -3,5 +3,15 @@
 	#error "Hardware not defined"
 #endif // HW_H
 
+
+void hal_gpio_pinMode(pin_t pin, enum HAL_GPIO_PINMODE);
+
+void hal_gpio_digitalWrite(pin_t pin, enum HAL_GPIO_OUTPUT);
+
+uint8_t hal_gpio_digitalRead(pin_t pin);
+
+uint16_t hal_gpio_analogRead(pin_t pin);
+
+
 #include "hal_gpio.h"
 
