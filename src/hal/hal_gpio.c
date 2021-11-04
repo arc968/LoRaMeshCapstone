09c4e9f3bf762885e056c68e1f7e5f7cdc5fd801
joshLariviere <joshlariv@gmail.com>
2021-11-04 14:45:27 -0400

    boilerplate

--- src/hal/hal_gpio.c
+++ src/hal/hal_gpio.c
@@ -3,15 +3,23 @@
 	#error "Hardware not defined"
 #endif // HW_H
 
+#include "hal_gpio.h"
 
-void hal_gpio_pinMode(pin_t pin, enum HAL_GPIO_PINMODE);
+void hal_gpio_pinMode(pin_t pin, enum HAL_GPIO_PINMODE) {
+	
+	#ifdef HW_ARDUINO
+		pinMode(pin, HAL_GPIO_PINMODE);	
+	#elif HW_IBUG_H
+	
+	#else
+		#error ""
+	#endif
+	
+}
 
 void hal_gpio_digitalWrite(pin_t pin, enum HAL_GPIO_OUTPUT);
 
 uint8_t hal_gpio_digitalRead(pin_t pin);
 
 uint16_t hal_gpio_analogRead(pin_t pin);
-
-
-#include "hal_gpio.h"
 
