20996a1556eea285d1c637e4ae633bda4ac161e8
arc968 <andrewcourtemanche968@gmail.com>
2021-11-16 13:48:18 -0500

    Update hal_i2c.c

--- src/hal/hal_i2c.c
+++ src/hal/hal_i2c.c
@@ -1,3 +1,27 @@
 #define HAL_LIB
 #include "hal_i2c.h"
 
+#ifdef HW_ARDUINO
+	#include <Wire.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+	void func() {
+		#ifdef HW_ARDUINO
+			Wire.begin();
+		#elif HW_IBUG_H
+		
+		#else
+			#error "Hardware not yet implemented"
+		#endif
+	}
+
+
+
+#ifdef __cplusplus
+}
+#endif
