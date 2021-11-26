6be25d4d2f4bb271f7edaa0f981ecdbcc9d804e8
joshLariviere <joshlariv@gmail.com>
2021-11-25 23:45:22 -0500

    Update drv_sensor_abstract.h

--- src/drivers/drv_sensor_abstract.h
+++ src/drivers/drv_sensor_abstract.h
@@ -1,7 +1,30 @@
 #ifndef DRV_SENSOR_ABSTRACT_H
 #define DRV_SENSOR_ABSTRACT_H
 
+#include <stdint.h>
+#include <stdlib.h>
 
+class drv_sensor_abstract {
+	
+	private:
+		bool initalized;
+		bool enabled;
+		
+	public:
+	
+		virtual drv_sensor_abstract() {initalized = false; isEnabled = false;}
+		virtual ~drv_sensor_abstract() {delete initalized; delete enabled;}
+		
+		virtual bool begin() = 0;
+		virtual bool end() = 0;
+		
+		bool isInitialized() {return initialized;}
+		bool isEnabled() {return enabled;}
+		
+		virtual void read() = 0;
+		virtual void write() = 0;
+	
+}
 
 
 #endif //DRV_SENSOR_ABSTRACT_H
