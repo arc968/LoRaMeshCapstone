7c95636b47a43b96f23b83cf4fa8948e5a014c10
joshLariviere <joshlariv@gmail.com>
2021-11-29 02:33:10 -0500

    sensor file set up

--- src/drivers/accelerometer/drv_accelerometer.h
+++ src/drivers/accelerometer/drv_accelerometer.h
@@ -1,6 +1,18 @@
 #ifndef DRV_ACCELEROMETER_H
 #define DRV_ACCELEROMETER_H
 
+#include "../drv_spi_sensor_abstract.h"
 
+class drv_accelerometer : drv_spi_sensor_abstract {
+	
+	private:
+		
+		
+		
+	public:
+		
+		
+		
+}
 
 #endif // DRV_ACCELEROMETER_H
