77cd0aa2e17da0e731b040f25302c2a5d84d917b
Andrew Courtemanche <andrewcourtemanche968@gmail.com>
2022-01-19 17:42:31 -0500

    Update hal_timer.h

--- src/hal/hal_timer.h
+++ src/hal/hal_timer.h
@@ -10,6 +10,10 @@ void hal_timer_delay(uint16_t msdelay);
 
 uint32_t hal_timer_millis(void);
 
+void hal_timer_reset(hal_timer_handle_t handle);
+
+uint32_t hal_timer_getTime(hal_timer_handle_t handle);
+
 #ifdef __cplusplus
 }
 #endif
