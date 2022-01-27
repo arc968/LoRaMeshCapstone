9eed99764e2d308a3cb1f2fa1dbf198ef64dd524
Andrew Courtemanche <andrewcourtemanche968@gmail.com>
2022-01-27 15:34:36 -0500

    prng function improvements

--- src/lib/misc/lib_misc.h
+++ src/lib/misc/lib_misc.h
@@ -24,21 +24,56 @@ inline uint32_t lib_misc_fastrange32(uint32_t word, uint32_t p) {
 	return (uint32_t)(((uint64_t)word * (uint64_t)p) >> 32);
 }
 
+inline uint16_t lib_misc_XORshiftLFSR16(uint16_t seed) {
+	seed ^= seed << 7;
+	seed ^= seed >> 9;
+	seed ^= seed << 8;
+	return seed;
+}
+
 inline uint32_t lib_misc_XORshiftLFSR32(uint32_t seed) {
-	seed ^= 0xB16B00B5;
 	seed ^= seed << 13;
 	seed ^= seed >> 17;
 	seed ^= seed << 5;
 	return seed;
 }
 
-inline uint16_t lib_misc_XORshiftLFSR16(uint16_t seed) {
-	//if (seed == 0) seed = 0xFFFF;
-	seed ^= 0x6042;
-	seed ^= seed << 7;
-	seed ^= seed >> 9;
-	seed ^= seed << 8;
+inline uint64_t lib_misc_XORshiftLFSR64(uint64_t seed) {
+	seed ^= seed << 12;
+	seed ^= seed >> 25;
+	seed ^= seed << 27;
 	return seed;
+}
+
+//THESE CONSTANTS ARE NOT VALIDATED
+uint16_t lib_misc_mix16(uint16_t val) {
+	val ^= 0xd27a; //arbitrary
+	val ^= val >> 7;
+	val *= 0x37f4;
+	val ^= val >> 5;
+	val *= 0xe91c;
+	val ^= val >> 8;
+	return val;
+}
+
+uint32_t lib_misc_mix32(uint32_t val) {
+	val ^= 0x8d0f937d; //arbitrary
+	val ^= val >> 16;
+	val *= 0x85ebca6b;
+	val ^= val >> 13;
+	val *= 0xc2b2ae35;
+	val ^= val >> 16;
+	return val;
+}
+
+uint64_t lib_misc_mix64(uint64_t val) {
+	val ^= 0x976ab62cda174f88; //arbitrary
+	val ^= val >> 30;
+	val *= 0xbf58476d1ce4e5b9;
+	val ^= val >> 27;
+	val *= 0x94d049bb133111eb;
+	val ^= val >> 31;
+	return val;
 }
 
 void shuffleArrU8(uint8_t * arr, uint16_t len, uint32_t seed);
