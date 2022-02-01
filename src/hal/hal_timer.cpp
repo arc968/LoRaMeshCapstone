8033c933ec8e2d8b264ac49db3e1c3558df5cada
arc968 <andrewcourtemanche968@gmail.com>
2022-02-01 14:13:09 -0500

    Update hal_timer.cpp

--- src/hal/hal_timer.cpp
+++ src/hal/hal_timer.cpp
@@ -1,24 +1,28 @@
 #define HAL_LIB
 #include "hal_timer.h"
 #include "TimerInterrupt_Generic.h"
+#include "ISR_Timer_Generic.h"
 
 #if defined(HW_MKRWAN1300_H)
 // Depending on the board, you can select SAMD21 Hardware Timer from TC3-TCC
 // SAMD21 Hardware Timer from TC3 or TCC
 // SAMD51 Hardware Timer only TC3
-SAMDTimer ITimer(TIMER_TC3);
+SAMDTimer ITimer0(TIMER_TC3);
+
+// Init SAMD_ISR_Timer
+// Each SAMD_ISR_Timer can service 16 different ISR-based timers
+ISR_Timer SAMD_ISR_Timer;
+
+void hal_timer_handler(void) {
+  SAMD_ISR_Timer.run();
+  //ITimer0.run();
+}
 
 #elif defined(HW_IBUG_H)
 
 #else
 	#error "Hardware not yet implemented"
-#endif
-
-ISR_Timer hal_ISR_Timer;
-
-void isr_hal_timer_handler(void) {
-  hal_ISR_Timer.run();
-}	
+#endif	
 
 static bool timerIsInitialized = false;
 
@@ -26,7 +30,7 @@ void hal_timer_init(void){
 	
 	if (!timerIsInitialized) {
 		
-		ITimer.attachInterruptInterval(1000 * 1000, isr_hal_timer_handler);
+		ITimer0.attachInterruptInterval(1000 * 1000, hal_timer_handler);
 		timerIsInitialized = true;
 	
 	}
@@ -59,7 +63,8 @@ uint32_t hal_timer_millis(void) {
 
 void hal_timer_setInterruptInterval(void (*isr)(void), uint32_t interval) {
 	
-	hal_ISR_Timer.setInterval(interval, isr);
+	//SAMD_ISR_Timer.setInterval(interval, isr);
+	ITimer0.setInterval(interval, isr);
 	
 }
 
