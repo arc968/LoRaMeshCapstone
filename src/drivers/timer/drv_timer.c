72e422deede2bb1b18929927dbe5f0ae3247e2ef
arc968 <andrewcourtemanche968@gmail.com>
2022-02-01 13:19:09 -0500

    Update drv_timer.c

--- src/drivers/timer/drv_timer.c
+++ src/drivers/timer/drv_timer.c
@@ -3,12 +3,13 @@
 
 volatile lib_datetime_interval_t monotonic_time = 0;
 
-void drv_timer_init(void) {
-	
-}
-
 void isr_incrementMonotonic(void) {
 	monotonic_time++;
+}
+
+void drv_timer_init(void) {
+	hal_timer_init();
+	hal_timer_setInterruptInterval(isr_incrementMonotonic, 1000); //once every ms
 }
 
 lib_datetime_interval_t drv_timer_getMonotonicTime(void) {
