cdf417072e498e78576729cdc3cc0d8eee71a21e
Andrew Courtemanche <andrewcourtemanche968@gmail.com>
2022-03-08 21:25:02 -0500

    spacing

--- src/hardware/lora/arduino/mkrwan1300/hw_lora_arduino_mkrwan1300.h
+++ src/hardware/lora/arduino/mkrwan1300/hw_lora_arduino_mkrwan1300.h
@@ -1,12 +1,11 @@
 #if !defined(HARDWARE_H_SCOPE) && !defined(HW_LORA_ARDUINO_MKRWAN1300_C)
 	#error "Do not directly include this header file, include hardware.h instead."
 #elif defined(HW_LORA_H) && !defined(HW_LORA_ARDUINO_MKRWAN1300_H)
-	#error "Multiple GPS hardware definitions."
+	#error "Multiple LoRa hardware definitions."
 #elif !defined(HW_LORA_ARDUINO_MKRWAN1300_H)
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 	#define HW_LORA_ARDUINO_SCOPE
 
 	#define HW_LORA_ARDUINO
