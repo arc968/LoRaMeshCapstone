22061b966b7c174906615cd216608bdb91d6f3f5
Andrew Courtemanche <andrewcourtemanche968@gmail.com>
2022-03-08 20:21:46 -0500

    MKRWAN1300 skeleton

---
+++ src/hardware/lora/arduino/MKRWAN1300/hw_lora_arduino_MKRWAN1300.c
@@ -0,0 +1,14 @@
+#define HW_LORA_ARDUINO_MKRWAN1300_C
+#include "hw_lora_arduino_MKRWAN1300.h"
+
+#include "../../../../drivers/lora/drv_lora.h"
+
+#ifdef HW_LORA_ARDUINO_MKRWAN1300_H
+
+void drv_lora_init(struct drv_lora_s * handle, pin_t pin) {
+	
+}
+
+#endif // HW_LORA_ARDUINO_MKRWAN1300_H
+
+#undef HW_LORA_ARDUINO_MKRWAN1300_C
