fbb4ca4cdb00340f66754f3811277b98115dde27
joshLariviere <joshlariv@gmail.com>
2022-03-09 19:05:55 -0500

    no gets

--- src/drivers/lora/drv_lora.h
+++ src/drivers/lora/drv_lora.h
@@ -59,13 +59,13 @@ struct drv_lora_packet_s {
 
 struct drv_lora_s {
 	pin_t pin;
-	enum drv_lora_mode_e mode;
+	/*enum drv_lora_mode_e mode;
 	enum drv_lora_region_e region;
 	enum drv_lora_bandwidth_e bandwidth;
 	enum drv_lora_spreadingFactor_e spreadingFactor;
 	enum drv_lora_codingRate_e codingRate;
 	uint16_t txpower;
-	uint16_t preamblelen;
+	uint16_t preamblelen;*/
 };
 
 void drv_lora_init(struct drv_lora_s * handle, enum drv_lora_region_e region, pin_t pin);
